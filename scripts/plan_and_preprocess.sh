71fb72bdab2f10c6a3eae2b5c93307ad90b43e05
Dimosthenis Chrysochoou <145491906+dchry@users.noreply.github.com>
2026-01-20 18:51:33 -0500

    Update plan_and_preprocess.sh

--- scripts/plan_and_preprocess.sh
+++ scripts/plan_and_preprocess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --mem=32G
-#SBATCH -c 16
+#SBATCH -c 8
 #SBATCH -t 03:00:00
 #SBATCH --error=/logs/err_%j.err
 #SBATCH --output=/logs/out_%j.out
@@ -12,16 +12,16 @@ nvidia-smi
 date
 
 # Activate the Miniconda base environment. Please adjust the path based on your local machine
-source ~/miniconda3/bin/activate
+source {your_conda_directory}/miniconda3/bin/activate
 
 conda activate dropout_nnUNet
-# pip install blosc2
+pip install blosc2
 
 
 cd {parent_directory}/dropout-nnunet-flair-t1/nnUNet #Please adjust {parent_directory}
 
 export nnUNet_raw="{parent_directory}/data/raw"
-export nnUNet_preprocessed="{parent_directory}/data//preprocessed"
+export nnUNet_preprocessed="{parent_directory}/data/preprocessed"
 export nnUNet_results="{parent_directory}/data/results"
 
 nnUNetv2_plan_and_preprocess -d 001 --verify_dataset_integrity
