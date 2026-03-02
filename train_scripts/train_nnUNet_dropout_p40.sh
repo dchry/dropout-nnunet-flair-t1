8c5d85659645dfd1950e86d784c44114c5fef2f2
Dimosthenis Chrysochoou <145491906+dchry@users.noreply.github.com>
2026-03-02 16:25:59 -0500

    Update train_nnUNet_dropout_p40.sh

--- train_scripts/train_nnUNet_dropout_p40.sh
+++ train_scripts/train_nnUNet_dropout_p40.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --mem=64G
-#SBATCH -c 32
-#SBATCH -t 00:10:00
-#SBATCH --error=/home/chrysochod/DRP_REPLICATION_FOR_GIT_PUBLISHING/logs/err_%j.err
-#SBATCH --output=/home/chrysochod/DRP_REPLICATION_FOR_GIT_PUBLISHING/logs/out_%j.out
+#SBATCH --mem=32G
+#SBATCH -c 16
+#SBATCH -t 16:00:00
+#SBATCH --error=/logs/err_%j.err
+#SBATCH --output=/logs/out_%j.out
 #SBATCH -p gpuq
 #SBATCH --gres=gpu:a100:1
 #SBATCH --job-name=drp_p40
@@ -13,16 +13,16 @@ nvidia-smi
 date
 
 # Activate the Miniconda base environment
-source /home/chrysochod/miniconda3/bin/activate
+source {your_conda_directory}/miniconda3/bin/activate
 
-conda activate dropout_nnunet
-export PYTHONNOUSERSITE=1
+conda activate drp_nnUNet
+# pip install blosc2
 
 
-cd /home/chrysochod/DRP_REPLICATION_FOR_GIT_PUBLISHING/dropout-nnunet-flair-t1
+cd {parent_directory}/dropout-nnunet-flair-t1/nnUNet
 
-export nnUNet_raw="/scr1/users/chrysochod/multimodal_scr/segmentation_replication_for_npj_git/data/raw"
-export nnUNet_preprocessed="/scr1/users/chrysochod/multimodal_scr/segmentation_replication_for_npj_git/data/preprocessed"
-export nnUNet_results="/scr1/users/chrysochod/multimodal_scr/segmentation_replication_for_npj_git/data/results"
+export nnUNet_raw="{parent_directory}/data/raw"
+export nnUNet_preprocessed="{parent_directory}/data/preprocessed"
+export nnUNet_results="{parent_directory}/data/results"
 
 nnUNetv2_train Dataset001_BrainTumor 3d_fullres 0 -tr nnUNetTrainerDropout_p40
